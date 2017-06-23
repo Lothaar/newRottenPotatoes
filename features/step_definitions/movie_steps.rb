455ebe46223ffcef4fbdac97e84a3e9fafd027db
Aaron Anderson <aaron.anderson.a7x@gmail.com>
2017-06-23 00:16:28 +0000

    Fixing stuff

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,51 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+   Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+  #fail "Unimplemented"
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  page.should have_content("#{e1}")
+  page.should have_content("#{e2}")
+  
+  movies = []
+  all('tbody tr td:first').each {|row| movies << row.text}
+  assert movies[0..1] == [e1, e2]
+  #fail "Unimplemented"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(", ")
+  ratings.each do |rating|
+    check "ratings_#{rating}" if uncheck.nil?
+    uncheck "ratings_#{rating}" if not uncheck.nil?
+  end
+  #fail "Unimplemented"
+end
+
+Then /I should see all the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  all('tbody tr td:first').each do |row|
+    page.should have_content(row.text)
+  end
+  #fail "Unimplemented"
+end
+
+
